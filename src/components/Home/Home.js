c04cca0aeb02368356c57db443d33bb205332140
as ahmed <rakibasrr13@gmail.com>
2021-01-09 18:52:55 +0300

    show our dream Data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import HeaderMain from '../Shared/HeaderMain/HeaderMain';
 import OurDream from './OurDream/OurDream';
 import WorkOut from './WorkOut/WorkOut';
 
 const Home = () => {
+    const [homePageAllData, setHomePageAllData] = useState(null);
+    const workOutData = homePageAllData ? homePageAllData.slice(0, 3) : '';
+    const ourDreamData = homePageAllData ? homePageAllData.slice(3,4) : '';
+    // const workOutData = homePageAllData ? homePageAllData.slice(0, 3) : '';
+
+    useEffect(() => {
+        fetch("http://localhost:4000/homePageAllData")
+            .then(res => res.json())
+            .then(data => setHomePageAllData(data))
+    }, [])
     return (
         <div>
             <HeaderMain />
-            <WorkOut />
-            <OurDream />
+            <WorkOut workOutData={workOutData} />
+            <OurDream ourDreamData={ourDreamData}/>
         </div>
     );
 };
--- src/components/Home/OurDream/OurDream.js
+++ src/components/Home/OurDream/OurDream.js
@@ -1,18 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import './OurDream.css'
+import { Col, Container, Row } from 'react-bootstrap';
 
-const OurDream = () => {
-    const [ourDream, setOurDream] = useState(null);
-    console.log(ourDream)
-    const _id = 4;
-    useEffect(() => {
-        fetch(`http://localhost:4000/ourDream/${_id}`)
-            .then(res => res.json())
-            .then(data => setOurDream(data))
-    }, [_id])
+const OurDream = ({ ourDreamData }) => {
     return (
-        <div>
-
-        </div>
+        <Container className="mt-5 pt-5">
+            {
+                ourDreamData ? <Row>
+                    <Col sm={12} md={6} lg={6} xl={6}>
+                        <img src={ourDreamData[0].OurDreamImg} className="img-fluid" alt="" />
+                    </Col>
+                    <Col sm={12} md={6} lg={6} xl={6} className="offset-1">
+                        <h1 className="pt-5 mt-4 text-uppercase"><span className="text-warning">{ourDreamData[0].title}</span>{ourDreamData[0].secondTitle}</h1>
+                        <p className="pt-4 our_dream_description">{ourDreamData[0].outDreamDescription}</p>
+                    </Col>
+                </Row> : <h2>loading...</h2>
+            }
+        </Container>
     );
 };
 
