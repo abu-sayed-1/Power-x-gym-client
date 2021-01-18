1311b3ac88585dfff040ca20a2753fdb39015ae0
as ahmed <rakibasrr13@gmail.com>
2021-01-18 15:26:44 +0300

    choose Course done

--- src/components/PricingPlans/ChooseCourse/ChooseCourse.js
+++ src/components/PricingPlans/ChooseCourse/ChooseCourse.js
@@ -1,5 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './ChooseCourse.css'
 import { useEffect } from 'react';
+import { Container, Card, CardGroup } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
+import NavBar from '../../Shared/Navbar/NavBar';
+import Footer from '../../Shared/Footer/Footer';
+
 
 // const ChooseCourseData = [
 //     {
@@ -48,7 +55,8 @@ import { useEffect } from 'react';
 // ]
 
 const ChooseCourse = () => {
-
+    const [course, setCourse] = useState(null);
+    console.log(course);
     // useEffect(() => {
     //      fetch('http://localhost:4000/pricingPlans', {
     //          method: 'POST',
@@ -57,10 +65,55 @@ const ChooseCourse = () => {
     //      })
     //      .then(res => console.log(res))
     // }, [])
+    useEffect(() => {
+        fetch('http://localhost:4000/chooseCourseData')
+            .then(res => res.json())
+            .then(data => setCourse(data));
+    }, [])
     return (
-        <div>
-
-        </div>
+        <>
+            <section className="header_main mb-5">
+                <div className=" overly">
+                    <NavBar />
+                    <h1 className="text-white text-center">PRICING PLANS</h1>
+                </div>
+            </section>
+            <Container className="mb-5">
+                <h1 className="text_color text-center">CHOOSE THE OFFER THAT SUITS YOU</h1>
+                <p className="text-center">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda <br/> illo quas consequatur unde facilis eligendi atque laudantium doloribus nobis aperiam.</p>
+                <CardGroup className="my-5 py-5">
+                    {
+                        course ? course.map(item => <Card sm={12} md={6} lg={4} xl={4} key={item._id}>
+                            <Card.Img className="choose_course_img" src={item.img} />
+                            <Card.ImgOverlay className="card_Img">
+                                <Card.Body>
+                                    <h6 className="text-center text_color">{item.billed}</h6>
+                                    <h2 className="text-center text-white">{item.title}</h2>
+                                    <h3 className="text-center price">{item.price}</h3>
+                                    <Card.Text className="text-center text-white p_l ">
+                                        <FontAwesomeIcon icon={faCheck} />    {item.advantage.mobile}
+                                    </Card.Text>
+                                    <Card.Text className="text-center text-white">
+                                        <FontAwesomeIcon icon={faCheck} />  {item.advantage.hosting}
+                                    </Card.Text>
+                                    <Card.Text className="text-center text-white">
+                                        <FontAwesomeIcon icon={faCheck} /> {item.advantage.custom}
+                                    </Card.Text>
+                                    <Card.Text className="text-center text-white">
+                                        <FontAwesomeIcon icon={faCheck} />  {item.advantage.outstanding}
+                                    </Card.Text>
+                                    <Card.Text className="text-center text-white p_l">
+                                        <FontAwesomeIcon icon={faCheck} />  {item.advantage.happy}
+                                    </Card.Text>
+                                    <button className="purchase_btn text-uppercase mt-3">{item.btnName}</button>
+                                </Card.Body>
+                            </Card.ImgOverlay>
+                        </Card>) : 'loading.....'
+                    }
+                </CardGroup>
+            </Container>
+            <Footer />
+        </>
     );
 };
 
