8250253678292caf5e5749273c15806c9902433c
as ahmed <rakibasrr13@gmail.com>
2021-01-18 10:23:49 +0300

    courses list 80% done

--- src/components/OurClasses/CoursesList/CoursesList.js
+++ src/components/OurClasses/CoursesList/CoursesList.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import './CoursesList.css'
 import { useState } from 'react';
 import { useEffect } from 'react';
-import { Container,Card, CardDeck } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import Footer from '../../Shared/Footer/Footer';
+import NavBar from '../../Shared/Navbar/NavBar';
+import { useHistory } from 'react-router-dom';
 
 // const courseLists = [
 //     {
@@ -38,8 +44,6 @@ import { Container,Card, CardDeck } from 'react-bootstrap';
 // ]
 const CoursesList = () => {
     const [coursesData, setCoursesData] = useState(null);
-    console.log(coursesData)
-
     // useEffect(() => {
     //     fetch('http://localhost:4000/coursesList', {
     //         method: 'POST',
@@ -53,24 +57,38 @@ const CoursesList = () => {
         fetch('http://localhost:4000/coursesByData')
             .then(res => res.json())
             .then(data => setCoursesData(data))
-    }, [])
+    }, []);
+
+    const history = useHistory();
+    const handleCourseDetail = (id) => {
+           console.log(id, 'course id ')
+    }
 
     return (
-        // <Container>
-            <CardDeck>
-                {
-                    coursesData ? coursesData.map(list =>
-                        <Card>
-                            <img src={list.img} alt="" className="w-100"/>
-                            {/* <Card.Footer> */}
-                                <button>{list.trainingName}</button>
-                            {/* </Card.Footer> */}
-                        </Card>
 
-                    ) : ''
-                }
-            </CardDeck>
-        // </Container>
+        <>
+            <section className="header_main mb-5">
+                <div className=" overly">
+                    <NavBar />
+                    <h1 className="text-white text-center">OUR CLASSES</h1>
+                </div>
+            </section>
+            <Container className="pt-5">
+                <Row>
+                    {
+                        coursesData ? coursesData.map(list =>
+                            <Col sm={12} md={6} lg={4} xl={4} key={list._id}>
+                                <img src={list.img} alt="" className="course_img" />
+                                <button onClick={() => handleCourseDetail(list.id)} className="courses_btn text-uppercase">{list.trainingName} <FontAwesomeIcon icon={faArrowRight} className="ml-2" /> </button>
+                            </Col>
+
+                        ) : ''
+                    }
+                </Row>
+            </Container>
+            <Footer />
+        </ >
+
     );
 };
 
