9e0addc55d1a3ef20b1b50b044925359e73ab541
as ahmed <rakibasrr13@gmail.com>
2021-02-23 23:06:17 +0300

    Show err

--- src/components/PricingPlans/PaymentsGateway/PaymentsGateway.js
+++ src/components/PricingPlans/PaymentsGateway/PaymentsGateway.js
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import './PaymentsGateway.css';
+import amex from '../../../images/ImageAndIcon/credit-cards_amex.png';
+import mastercard from '../../../images/ImageAndIcon/credit-cards_mastercard.png';
+import visaCard from '../../../images/ImageAndIcon/credit-cards_visa.png';
 import { Col, Container, Row } from 'react-bootstrap';
 import Footer from '../../Shared/Footer/Footer';
 import NavBar from '../../Shared/Navbar/NavBar';
@@ -7,8 +11,13 @@ import { loadStripe } from "@stripe/stripe-js";
 import { Elements, } from "@stripe/react-stripe-js";
 import CheckoutForm from './Paypal/CheckoutForm/CheckoutForm';
 import { toast } from 'react-toastify';
-
 toast.configure()
+
+const creditCardImages = [
+    { id: 1, img: mastercard },
+    { id: 2, img: visaCard },
+    { id: 3, img: amex },
+];
 const PaymentsGateway = () => {
     const stripePromise = loadStripe("pk_test_51HaKX2FWzFyXdW5KjdYVQtPEcdPZOSLq0nvfi4MfePscvZAop5VwXrGvH9Z0XjenRtpUwNFsX07um8rLzI8yrrB600opOV9Hw9");
 
@@ -28,48 +37,59 @@ const PaymentsGateway = () => {
                 </div>
             </section>
             <Container>
-                <div className="form-check">
-                    <input onClick={() => setCheckout({ credit: "creditCard" })} className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
-                    <label className="form-check-label" for="inlineRadio1">
-                        Credit Card
-                    </label>
+                <div className="form-check card_content">
+                    <Row>
+                        <Col xs={8}>
+                            <input onClick={
+                                () => setCheckout({ credit: "creditCard" })}
+                                className="form-check-input check_input" type="radio"
+                                name="inlineRadioOptions" id="inlineRadio1"
+                                value="option1" />
+                            <label className="form-check-label" htmlFor="inlineRadio1">
+                                <h4>Credit Card</h4>
+                            </label>
+                            <p className="text_color mt-2">Safe money transfer using your bank account. Visa, Maestro, Discover, American Express</p>
+                        </Col>
+                        <Col>
+                            <div className="pl-md-5">
+                                {
+                                    creditCardImages.map(
+                                        cardImage => <img className="pl-2" key={cardImage.id} src={cardImage.img} alt="" />
+                                    )
+                                }
+                            </div>
+                        </Col>
+                    </Row>
                     <Elements stripe={stripePromise}>
                         <CheckoutForm />
-                        
                     </Elements>
-                    <form onSubmit={handleSubmit} style={{ margin: '40px' }}>
-                        <CardElement />
-                        <button type="submit">pay</button>
-                    </form>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate qui veniam, neque eaque fugiat molestiae molestias eligendi architecto repellat sint</p>
                 </div>
-                <Row className="border p-5">
+                <Row className="p-5 mt-5 m-0 paypal_content">
                     <Col>
                         <div className="form-check">
                             <input onClick={() => setCheckout({ paypal: "paypal" })} className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
                             <label className="form-check-label" htmlFor="inlineRadio2">
-                                Paypal
-                             </label>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate qui veniam, neque eaque fugiat molestiae molestias eligendi architecto repellat sint</p>
+                                <h4>Paypal</h4>
+                            </label>
+                            <p className="pt-4 text_color">You well be redirected to PayPal website to complete your purchase securely.</p>
                         </div>
                     </Col>
                     <Col xs={4}>
                         {checkout.paypal ? <Paypal /> : <img src="https://tse3.mm.bing.net/th?id=OIP.5WodCMcfWx9Gq8jWPTrmGQHaC4&pid=Api&P=0&w=474&h=185" width='200' className="d-flux justify-content-center" alt="" />}
-
                     </Col>
                 </Row>
                 {
                     !checkout.credit && !checkout.paypal &&
                     <button onClick={() =>
-                          toast.error('Please Choose payment gateway !', {position: toast.POSITION.TOP_CENTER})
-                        }
+                        toast.error('Please Choose payment gateway !', { position: toast.POSITION.TOP_CENTER })
+                    }
                     >null</button>
                 }
 
                 {
                     checkout.paypal && <button onClick={() =>
-                         toast.error('Please Choose the Paypal or Debit/Credit Card button !', {position: toast.POSITION.TOP_CENTER})
-                        }
+                        toast.error('Please Choose the Paypal or Debit/Credit Card button !', { position: toast.POSITION.TOP_CENTER })
+                    }
                     >Paypal</button>
                 }
 
--- src/components/PricingPlans/PaymentsGateway/Paypal/CheckoutForm/CheckoutForm.js
+++ src/components/PricingPlans/PaymentsGateway/Paypal/CheckoutForm/CheckoutForm.js
@@ -1,27 +1,44 @@
-// import React from 'react';
+import React, { useState } from 'react';
 import {
-    CardElement,
+    CardCvcElement,
+    CardExpiryElement,
+    CardNumberElement,
     useStripe,
     useElements
 } from "@stripe/react-stripe-js";
 import { useHistory } from 'react-router-dom';
 import axios from "axios";
 import { toast } from 'react-toastify';
+import { Button, Col, Row, Spinner } from 'react-bootstrap';
 
 toast.configure()
 const CheckoutForm = () => {
+    const [process, setProcess] = useState(false);
+    const [checkoutFormEmptyOrNot, setCheckoutFormEmptyOrNot] = useState({
+        cardNumber:'',
+        expiryDate:'',
+        cvcCode:''
+    });
+    console.log(checkoutFormEmptyOrNot)
     const history = useHistory()
     const stripe = useStripe();
     const elements = useElements();
 
     const handleSubmit = async (event) => {
+        //  console.log(event.input.value == empty)
         event.preventDefault();
         const { error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
-            card: elements.getElement(CardElement)
+            card: elements.getElement(CardNumberElement, CardExpiryElement, CardCvcElement),
         });
+        setProcess(paymentMethod, true)
+        if (!paymentMethod) {
+            toast.warning('Please enter your card information!', { position: toast.POSITION.TOP_CENTER })
 
+        }
+        // setEmptyFrom(!paymentMethod)
         if (!error) {
+            console.log(paymentMethod)
             try {
                 const { id } = paymentMethod;
                 const response = await axios.post(
@@ -33,28 +50,52 @@ const CheckoutForm = () => {
                 );
 
                 if (response.data.success === true) {
-                    history.push( '/MembershipCreated');
-                } 
+                    history.push('/MembershipCreated');
+                }
                 if (response.data.success === false) {
-                    toast.error(response.data.message, {position: toast.POSITION.TOP_CENTER})
+                    setProcess(false)
+                    toast.error(response.data.message, { position: toast.POSITION.TOP_CENTER })
 
                 }
 
             }
-             catch (error) {
+            catch (error) {
                 console.log(error.message)
             }
         }
     };
 
-    // return (
-    //     <div>
-    //         <form onSubmit={handleSubmit} style={{ margin: '40px' }}>
-    //             <CardElement />
-    //             <button type="submit">pay</button>
-    //         </form>
-    //     </div>
-    // );
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="" className="input_name pt-5 mt-md-2">CARD NUMBER</label>
+                <CardNumberElement onChange={e => setCheckoutFormEmptyOrNot({ cardNumber: e.target.value })} className="payment_inputs" />
+                <Row>
+                    <Col>
+                        <label htmlFor="" className="input_name pt-4">EXPIRY DATE</label>
+                        <CardExpiryElement onChange={e => setCheckoutFormEmptyOrNot({ expiryDate: e.target.value })} className="payment_inputs" />
+                    </Col>
+                    <Col>
+                        <label htmlFor="" className="input_name pt-4">CVC CODE</label>
+                        <CardCvcElement onChange={e => setCheckoutFormEmptyOrNot({ cvcCode: e.target.value })} className="payment_inputs" />
+                    </Col>
+                </Row>
+                {process ?
+                    <Button variant="primary" disabled>
+                        <Spinner
+                            as="span"
+                            animation="grow"
+                            size="sm"
+                            role="status"
+                            aria-hidden="true"
+                        />
+                      Process...
+                    </Button>
+                    : <button type="submit">pay</button>}
+
+            </form>
+        </div>
+    );
 };
 
-// export default CheckoutForm;
+export default CheckoutForm;
